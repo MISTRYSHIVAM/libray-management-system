01f359c30688c8a5333c68e51f401f0772fd102f
shivam <shivammistry2003@gmail.com>
2024-08-11 15:39:51 +0530

    add test 2 , 3 ,4 to borrowbook for isbn validation

--- src/test/libraryBorrowbook.test.ts
+++ src/test/libraryBorrowbook.test.ts
@@ -5,10 +5,21 @@ describe("book borrow feature testing", () => {
     beforeEach(() => {
         library = new Library();
     });
+
     test("1. user should borrow the book", () => {
         let isbn = "0123456789";
         expect(() => {
             library.borrowBook(isbn);
         }).not.toThrow();
+    });
+
+    let invalidIsbn = ["", "sdgy4587", "123654"];
+
+    invalidIsbn.forEach((isbn, index) => {
+        test(`${index + 2}. throw exception when isbn is invalid`, () => {
+            expect(() => {
+                library.borrowBook(isbn);
+            }).toThrow();
+        });
     });
 });
