5f8749e93ccd8dd0fed293904384704f92c28740
shivam <shivammistry2003@gmail.com>
2024-08-11 16:49:52 +0530

    add test 2 , 3 , 4 in return book to check isbn is valid or not

--- src/test/libraryReturnbook.test.ts
+++ src/test/libraryReturnbook.test.ts
@@ -2,6 +2,7 @@ import Library from "../app/libraryUtility";
 
 describe("book returing fetures testing", () => {
     let library: Library;
+
     beforeEach(() => {
         library = new Library();
     });
@@ -11,5 +12,15 @@ describe("book returing fetures testing", () => {
         expect(() => {
             library.returnBook(isbn);
         }).not.toThrow();
+    });
+
+    let invalidIsbn = ["", "sdgy458725", "123654"];
+
+    invalidIsbn.forEach((isbn, index) => {
+        test(`${index + 2}. throw exception when isbn is invalid`, () => {
+            expect(() => {
+                library.returnBook(isbn);
+            }).toThrow();
+        });
     });
 });
